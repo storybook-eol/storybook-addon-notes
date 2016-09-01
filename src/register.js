ad40871ba836cfbec3cdcfef168f34b3229e1c87
Muhammed Thanish <mnmtanish@gmail.com>
2016-09-01 18:05:24 +0530

    Implement reading notes from database

--- src/register.js
+++ src/register.js
@@ -1,32 +1,48 @@
 import React from 'react';
 import addons from '@kadira/storybook-addons';
 
+// addons, panels and events get unique names using a prefix
+export const ADDON_ID = 'kadirahq/storybook-addon-notes';
+export const PANEL_ID = `${ADDON_ID}/notes-panel`;
+export const COLLECTION_ID = `${ADDON_ID}/notes-collection`;
+
 const styles = {
-  notesPanel: {
-    margin: 10,
+  wrapper: {
+    flex: 1,
+    display: 'flex',
+    position: 'relative',
+  },
+  textarea: {
+    flex: 1,
+    outline: 'none',
+    border: 'none',
+    resize: 'none',
+    padding: 10,
     fontFamily: 'Arial',
     fontSize: 14,
     color: '#444',
-    width: '100%',
-    overflow: 'auto',
+    lineHeight: 1.5,
   },
+  errorText: {
+    color: '#C62828',
+  }
 };
 
 export class Notes extends React.Component {
   constructor(...args) {
     super(...args);
-    this.state = { text: '' };
+    this.state = { modified: false, loading: false, errored: false, text: '' };
     this.onAddNotes = this.onAddNotes.bind(this);
+    this.onTextChange = this.onTextChange.bind(this);
   }
 
   componentDidMount() {
-    const { channel, api } = this.props;
-    // Listen to the notes and render it.
-    channel.on('kadira/notes/add_notes', this.onAddNotes);
-
     // Clear the current notes on every story change.
-    this.stopListeningOnStory = api.onStory(() => {
-      this.onAddNotes('');
+    this.stopListeningOnStory = this.props.api.onStory((kind, story) => {
+      this.setState({ errored: false, loading: true, text: '' });
+      this.getStoryNotes(kind, story)
+        .then(text => this.onAddNotes(text))
+        .catch(err => this.onNetError(err));
     });
   }
 
@@ -35,40 +51,55 @@ export class Notes extends React.Component {
     if (this.stopListeningOnStory) {
       this.stopListeningOnStory();
     }
-
     this.unmounted = true;
-    const { channel } = this.props;
-    channel.removeListener('kadira/notes/add_notes', this.onAddNotes);
+  }
+
+  getStoryNotes(kind, story) {
+    const db = addons.getDatabase();
+    return db.getCollection(COLLECTION_ID)
+      .get({sbKind: kind, sbStory: story})
+      .then(res => res[0] && res[0].notes || '');
   }
 
   onAddNotes(text) {
-    this.setState({ text });
+    this.setState({ errored: false, modified: false, loading: false, text });
+  }
+
+  onNetError(err) {
+    const text = `Error: ${err.message}`;
+    this.setState({ errored: true, modified: false, loading: false, text });
+  }
+
+  onTextChange(e) {
+    this.setState({ modified: true, text: e.target.value });
   }
 
   render() {
-    const { text } = this.state;
-    const textAfterFormatted = text ? text.trim().replace(/\n/g, '<br />') : '';
+    const { text, errored, loading } = this.state;
+    const textStyle = Object.assign({}, styles.textarea);
+    if (errored) {
+      Object.assign(textStyle, styles.errorText);
+    }
 
     return (
-      <div style={styles.notesPanel}>
-        <div dangerouslySetInnerHTML={{ __html: textAfterFormatted }} />
+      <div style={styles.wrapper}>
+        <textarea style={textStyle} value={text} onChange={this.onTextChange} />
       </div>
     );
   }
 }
 
 Notes.propTypes = {
-  channel: React.PropTypes.object,
   api: React.PropTypes.object,
 };
 
 // Register the addon with a unique name.
-addons.register('kadira/notes', (api) => {
+addons.register(ADDON_ID, (api) => {
   // Also need to set a unique name to the panel.
-  addons.addPanel('kadira/notes/panel', {
+  addons.addPanel(PANEL_ID, {
     title: 'Notes',
     render: () => (
-      <Notes channel={addons.getChannel()} api={api} />
+      <Notes api={api} />
     ),
   });
 });
